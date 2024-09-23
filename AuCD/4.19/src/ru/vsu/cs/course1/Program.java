c42ee238c5c200c0f3a9363f776781cc481946b1
Nikolay Vereschagin <115875329+vrshgnick@users.noreply.github.com>
2024-09-23 16:53:37 +0300

    Add files via upload

---
+++ AuCD/4.19/src/ru/vsu/cs/course1/Program.java
@@ -0,0 +1,101 @@
+package ru.vsu.cs.course1;
+
+import org.apache.commons.cli.CommandLine;
+import ru.vsu.cs.util.ArrayUtils;
+import ru.vsu.cs.util.SwingUtils;
+
+import javax.swing.*;
+import java.io.PrintStream;
+import java.util.Locale;
+
+
+public class Program {
+
+    public static class CmdParams {
+        public String inputFile;
+        public String outputFile;
+        public boolean reverseRows;
+        public boolean reverseColumns;
+        public boolean error;
+        public boolean help;
+        public boolean window;
+    }
+
+    public static CmdParams parseArgs(String[] args) {
+        CmdParams params = new CmdParams();
+        if (args.length > 0) {
+            if (args[0].equals("--help")) {
+                params.help = true;
+                return params;
+            }
+            if (args[0].equals("--window")) {
+                params.window = true;
+                return params;
+            }
+            if (!args[0].equals("-r") && !args[0].equals("-c")) {
+                params.error = true;
+                params.help = true;
+                return params;
+            }
+            if (args[0].equals("-r")) {
+                params.reverseRows = true;
+            } else {
+                params.reverseColumns = true;
+            }
+            if (args.length < 2) {
+                params.help = true;
+                params.error = true;
+                return params;
+            }
+
+            params.inputFile = args[1];
+            if (args.length > 2) {
+                params.outputFile = args[2];
+            }
+        } else {
+            params.help = true;
+            params.error = true;
+        }
+        return params;
+    }
+
+    public static void winMain() throws Exception {
+        //SwingUtils.setLookAndFeelByName("Windows");
+        Locale.setDefault(Locale.ROOT);
+        //UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        SwingUtils.setDefaultFont("Microsoft Sans Serif", 18);
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new FrameMain().setVisible(true);
+            }
+        });
+    }
+
+    public static void main(String[] args) throws Exception {
+        CmdParams params = parseArgs(args);
+        if (params.help) {
+            PrintStream out = params.error ? System.err : System.out;
+            out.println("Usage:");
+            out.println("  <cmd> args <input-file> (<output-file>)");
+            out.println("    -r  // reverse rows");
+            out.println("    -c  // reverse columns");
+            out.println("  <cmd> --help");
+            out.println("  <cmd> --window  // show window");
+            System.exit(params.error ? 1 : 0);
+        }
+        if (params.window) {
+            winMain();
+        } else {
+            int[][] arr2 = ArrayUtils.readIntArray2FromFile(params.inputFile);
+            if (arr2 == null) {
+                System.err.printf("Can't read array from \"%s\"%n", params.inputFile);
+                System.exit(2);
+            }
+            PrintStream out = (params.outputFile != null) ? new PrintStream(params.outputFile) : System.out;
+            out.println(ArrayUtils.toString(arr2));
+            out.close();
+        }
+    }
+}
